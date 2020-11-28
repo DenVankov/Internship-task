978723f399739868f5fe82a4310c3864f4ebce09
Denis <ivankovden99@gmail.com>
2020-11-28 19:13:53 +0300

    Update loop.py

--- loop.py
+++ loop.py
@@ -6,8 +6,7 @@ import requests
 url = 'http://localhost:5000/upload/metrics/'
 
 while True:
-    names = [1, 2, 3, 4, 5]
-    for name in names:
+    for name in range(1, 11):
         value = round(random.uniform(10.5, 100.5), 4)
         data = {"metric_value": value, "metric_name": f'metric_{name}'}
         resp = requests.post(url, json=data)
